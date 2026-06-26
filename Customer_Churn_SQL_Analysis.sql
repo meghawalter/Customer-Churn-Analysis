bd2c1dbcc91cd0ac8352057138633ec2a522b9ff
meghawalter <48826447+meghawalter@users.noreply.github.com>
2026-06-26 10:16:31 +0530

    Add files via upload

---
+++ Customer_Churn_SQL_Analysis.sql
@@ -0,0 +1,31 @@
+
+SELECT COUNT(*) AS TotalCustomers
+FROM dbo.CustomerChurn;
+
+SELECT
+    Churn,
+    COUNT(*) AS Customers
+FROM dbo.CustomerChurn
+GROUP BY Churn;
+
+SELECT
+    Contract,
+    Churn,
+    COUNT(*) AS Customers
+FROM dbo.CustomerChurn
+GROUP BY Contract, Churn
+ORDER BY Contract;
+
+SELECT
+    Churn,
+    AVG(CAST(MonthlyCharges AS DECIMAL(10,2))) AS AvgMonthlyCharges
+FROM dbo.CustomerChurn
+GROUP BY Churn;
+
+SELECT
+    InternetService,
+    Churn,
+    COUNT(*) AS Customers
+FROM dbo.CustomerChurn
+GROUP BY InternetService, Churn
+ORDER BY InternetService;
